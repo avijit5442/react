e0bea5c3616804b6a440b56b1244a707482fdb77
Avijit Behera <avijitbehera01@gmail.com>
2022-10-15 20:46:45 +0530

    1st

--- src/Portfolio/Navbar.js
+++ src/Portfolio/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Navbar() {
   return ( 
     <div> 
-              <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+              <nav className="navbar navbar-expand-lg bg-dark navbar-dark"> 
     <div className="container-fluid">
     <a className="navbar-brand">AVIJIT</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
